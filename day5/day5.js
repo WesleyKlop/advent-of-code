e33a5e11e167f940be5c36cf14f9fb79eb62bd64
Wesley Klop <wesley19097@gmail.com>
2019-12-05 21:58:18 +0100

    Add day 5

---
+++ day5/day5.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+const fs = require('fs')
+
+// Load and parse the input file
+const inputIntList = fs
+  .readFileSync(`${__dirname}/input.txt`, 'utf8')
+  .trim()
+  .split(',')
+  .map(val => parseInt(val))
+
+// Clone the list so you always have a fresh input
+const cloneInstructions = () => [...inputIntList]
+
+const DEBUG = true
+const debug = function() {
+  DEBUG && console.log(...arguments)
+}
+
+const OP_ADD = 1
+const OP_TIMES = 2
+const OP_INPUT = 3
+const OP_OUTPUT = 4
+const OP_JIT = 5
+const OP_JIF = 6
+const OP_LT = 7
+const OP_EQ = 8
+const OP_EXIT = 99
+const PARAM_MODE_POSITION = '0'
+const PARAM_MODE_IMMEDIATE = '1'
+
+const readParameter = (list, mode = PARAM_MODE_POSITION, address) => {
+  switch (mode) {
+    case PARAM_MODE_POSITION:
+      return list[address]
+    case PARAM_MODE_IMMEDIATE:
+      return address
+    default:
+      throw new Error('Invalid parameter mode ' + mode)
+  }
+}
+
+const parseOpcode = raw => {
+  const opcode = raw.toString().slice(-2)
+  const mode = raw
+    .toString()
+    .slice(0, -2)
+    .split('')
+
+  return [mode, parseInt(opcode, 10)]
+}
+
+// Execute the instructions on a list
+const executeInstructions = (instructions, input) => {
+  let jump = 4
+  for (let i = 0; i < instructions.length; i += jump) {
+    // Slice the instruction out of the list
+    const [rawOpcode, loc1, loc2, reg] = instructions.slice(i, i + 4)
+    const [modes, opcode] = parseOpcode(rawOpcode)
+
+    switch (opcode) {
+      case OP_ADD:
+        instructions[reg] =
+          readParameter(instructions, modes.pop(), loc1) +
+          readParameter(instructions, modes.pop(), loc2)
+        jump = 4
+        break
+      case OP_TIMES:
+        instructions[reg] =
+          readParameter(instructions, modes.pop(), loc1) *
+          readParameter(instructions, modes.pop(), loc2)
+        jump = 4
+        break
+      case OP_INPUT:
+        instructions[loc1] = input.pop()
+        jump = 2
+        break
+      case OP_OUTPUT:
+        console.log('[OUTPUT] ' + instructions[loc1])
+        jump = 2
+        break
+      case OP_JIT:
+        jump = 3
+        if (readParameter(instructions, modes.pop(), loc1) !== 0) {
+          i = readParameter(instructions, modes.pop(), loc2)
+          jump = 0
+        }
+        break
+      case OP_JIF:
+        jump = 3
+        if (readParameter(instructions, modes.pop(), loc1) === 0) {
+          i = readParameter(instructions, modes.pop(), loc2)
+          jump = 0
+        }
+        break
+      case OP_LT:
+        instructions[reg] =
+          readParameter(instructions, modes.pop(), loc1) <
+          readParameter(instructions, modes.pop(), loc2)
+            ? 1
+            : 0
+        jump = 4
+        break
+      case OP_EQ:
+        instructions[reg] =
+          readParameter(instructions, modes.pop(), loc1) ===
+          readParameter(instructions, modes.pop(), loc2)
+            ? 1
+            : 0
+        jump = 4
+        break
+      case OP_EXIT:
+        jump = 0
+        return instructions
+      default:
+        throw new Error('Invalid opcode ' + opcode)
+    }
+  }
+  throw new Error('No end of program found')
+}
+
+executeInstructions(cloneInstructions(), [1])
+console.log('Answer 1 ^^^^^^^')
+executeInstructions(cloneInstructions(), [5])
+console.log('Answer 2 ^^^^^^^')
