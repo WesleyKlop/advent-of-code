92c137d832d3d2ef94d81cc43679910031f35151
Wesley Klop <wesley19097@gmail.com>
2019-12-06 08:09:33 +0100

    Day 6 part 2

--- day6/day6.js
+++ day6/day6.js
@@ -8,9 +8,6 @@ const inputIntList = fs
   .split('\n')
   .map(val => val.split(')'))
 
-// Map of child and its parent
-const map = new Map()
-
 const traverseMap = (map, child, orbits = 0) => {
   if (!map.has(child)) {
     return orbits
@@ -18,12 +15,47 @@ const traverseMap = (map, child, orbits = 0) => {
   return traverseMap(map, map.get(child), orbits + 1)
 }
 
-inputIntList.forEach(([parent, child]) => {
-  map.set(child, parent)
-})
+const findPath = (map, child, set = []) => {
+  set.push(child)
+  if (!map.has(child)) {
+    return set
+  }
+  return findPath(map, map.get(child), set)
+}
 
-const answer1 = inputIntList.reduce((total, [, child]) => {
-  return total + traverseMap(map, child)
-}, 0)
+const listToMap = list => {
+  const map = new Map()
+  list.forEach(([parent, child]) => {
+    map.set(child, parent)
+  })
+  return map
+}
+// Map of child and its parent
+const inputMap = listToMap(inputIntList)
 
-console.log(answer1)
+const answerOne = map =>
+  inputIntList.reduce((total, [, child]) => {
+    return total + traverseMap(map, child)
+  }, 0)
+
+const intersect = (a, b) => a.find(loc1 => b.some(loc2 => loc1 === loc2))
+
+const subPath = (path, loc) => {
+  return path.slice(1, path.indexOf(loc) + 1)
+}
+
+const findOrbitalTransferCost = map => {
+  const youPath = findPath(map, 'YOU')
+  const sanPath = findPath(map, 'SAN')
+
+  const intersection = intersect(youPath, sanPath)
+
+  return (
+    subPath(youPath, intersection).length +
+    subPath(sanPath, intersection).length -
+    2
+  )
+}
+
+console.log(answerOne(inputMap))
+console.log(findOrbitalTransferCost(inputMap))
