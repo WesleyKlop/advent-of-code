83a642b9e8dee49bbe18fdd4f07cc6dee0cd9b26
Wesley Klop <wesley19097@gmail.com>
2020-12-01 11:28:36 +0100

    Add lazy reading of files

--- app/Solvers/UsesInput.php
+++ app/Solvers/UsesInput.php
@@ -3,6 +3,7 @@
 
 namespace App\Solvers;
 
+use Illuminate\Support\LazyCollection;
 use Illuminate\Support\Str;
 use Illuminate\Support\Stringable;
 
@@ -10,13 +11,34 @@ trait UsesInput
 {
     protected function read(?string $year = null, ?string $day = null, string $name = 'input.txt'): Stringable
     {
-        if($year === null || $day === null) {
+        if ($year === null || $day === null) {
             preg_match("/Y(\d+)\\\Day(\d+)\\\/", static::class, $matches);
             [, $year, $day] = $matches;
         }
-        $path = resource_path(sprintf("inputs/%s/%s/%s", $year, $day, $name));
-        $contents = file_get_contents($path);
+        $path = $this->getPath($year, $day, $name);
 
-        return Str::of($contents)->trim();
+        return Str::of(file_get_contents($path))->trim();
+    }
+
+    protected function readLazy(string $year, string $day, string $name = 'input.txt'): LazyCollection
+    {
+        $path = $this->getPath($year, $day, $name);
+
+        return LazyCollection::make(function () use ($path) {
+            try {
+                $handle = fopen($path, 'rb');
+
+                while (($line = fgets($handle)) !== false) {
+                    yield trim($line);
+                }
+            } finally {
+                fclose($handle);
+            }
+        });
+    }
+
+    private function getPath(string $year, string $day, string $name): string
+    {
+        return resource_path(sprintf("inputs/%s/%s/%s", $year, $day, $name));
     }
 }
