a34d505584c13e8fab7651195846ba4f73c4e548
Wesley Klop <wesley19097@gmail.com>
2020-12-06 12:03:59 +0100

    Day 6 answers

---
+++ app/Solvers/Y2020/Day6/Solver.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Solvers\Y2020\Day6;
+
+use App\Contracts\Solution;
+use App\Solutions\PrimitiveValueSolution;
+use App\Solvers\AbstractSolver;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class Solver extends AbstractSolver
+{
+    protected function solvePartOne(): Solution
+    {
+        $uniqueAnswersPerGroup = $this
+            ->read('2020', '6')
+            ->explode("\n\n")
+            ->map(function (string $group) {
+                // Map each group to members with an array of their given answers.
+                return Str
+                    ::of($group)
+                    ->explode("\n")
+                    ->map(fn (string $answers) => str_split($answers))
+                    // Map a dictionary containing the keys of every answer given
+                    ->flatten()
+                    ->flip();
+            })
+            ->sum(fn (Collection $group) => $group->count());
+
+        return new PrimitiveValueSolution($uniqueAnswersPerGroup);
+    }
+
+    protected function solvePartTwo(): Solution
+    {
+        $uniqueAnswersPerGroup = $this
+            ->read('2020', '6')
+            ->explode("\n\n")
+            // Create a dictionary containing answers that everyone answers yes to
+            ->map(fn (string $group) => Str::of($group)
+                ->explode("\n")
+                // Create a dictionary containing answers that everyone answers yes to
+                ->map(fn (string $answers) => str_split($answers))
+                ->reduce(fn (
+                    ?Collection $group,
+                    array $member
+                ) => $group === null ? collect($member) : $group->intersect($member)))
+            ->sum(fn (Collection $group) => $group->count());
+
+        return new PrimitiveValueSolution($uniqueAnswersPerGroup);
+    }
+}
