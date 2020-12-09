fca8730e386896707554a671186d279362bb491d
Wesley Klop <wesley19097@gmail.com>
2020-12-09 10:23:23 +0100

    Day 9 part one

--- app/Solvers/Y2020/Day9/Solver.php
+++ app/Solvers/Y2020/Day9/Solver.php
@@ -5,25 +5,36 @@ namespace App\Solvers\Y2020\Day9;
 
 
 use App\Contracts\Solution;
+use App\Exceptions\AnswerNotFoundException;
+use App\Solutions\PrimitiveValueSolution;
 use App\Solutions\TodoSolution;
 use App\Solvers\AbstractSolver;
 
 class Solver extends AbstractSolver
 {
+    private const PREAMBLE = 25;
+    private const TARGET = 57195069;
 
     protected function solvePartOne(): Solution
     {
-        $source = $this->readLazy('2020', '9', 'test.txt')->map(fn(string $line) => intval($line));
+        $source = $this
+            ->readLazy('2020', '9')
+            ->map(fn(string $line) => (int)$line);
         $sourceCount = $source->count();
-        // 5 => Offset
-        for($i = 5; $i < $sourceCount; $i++) {
+
+        for($i = self::PREAMBLE; $i < $sourceCount; $i++) {
             // Consider the previous offset values, one of them MUST be a sum equal the current number
             $verify = $source->get($i);
+            $combinations = $source
+                ->slice($i - self::PREAMBLE, self::PREAMBLE)
+                ->collect();
+            $combinations = $combinations->crossJoin($combinations);
+            if($combinations->every(fn(array $combo) => $combo[0] + $combo[1] !== $verify)) {
+                return new PrimitiveValueSolution($verify);
+            }
         }
 
-
-
-        return new TodoSolution();
+        throw new AnswerNotFoundException("Could not find any number that is vulnerable");
     }
 
     protected function solvePartTwo(): Solution
