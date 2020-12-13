24112e517f9845a81e9375958f8f86ee320d5445
Wesley Klop <wesley19097@gmail.com>
2020-12-13 03:32:15 +0100

    Day 10 part one

---
+++ app/Solvers/Y2020/Day10/Solver.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Solvers\Y2020\Day10;
+
+
+use App\Contracts\Solution;
+use App\Solutions\PrimitiveValueSolution;
+use App\Solutions\TodoSolution;
+use App\Solvers\AbstractSolver;
+use Illuminate\Support\Collection;
+
+class Solver extends AbstractSolver
+{
+    private function getAdapters(): Collection
+    {
+        return $this
+            ->read('2020', '10', 'input.txt')
+            ->explode("\n")
+            ->map(fn (string $val) => (int) $val);
+    }
+
+    protected function solvePartOne(): Solution
+    {
+        $jolts = 0;
+        $adapters = $this->getAdapters()->sort();
+        $diffs = [
+            1 => 0,
+            3 => 1
+        ];
+        $prevAdapter = 0;
+        foreach ($adapters as $adapter) {
+            $diff = $adapter - $prevAdapter;
+            $diffs[$diff]++;
+            $jolts += $adapter;
+            $prevAdapter = $adapter;
+        }
+
+        return new PrimitiveValueSolution(array_product($diffs));
+    }
+
+    protected function solvePartTwo(): Solution
+    {
+        return new TodoSolution();
+    }
+}
