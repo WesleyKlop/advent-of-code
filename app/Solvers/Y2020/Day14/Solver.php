7ce99c4fd6ff5b53dfa953677c00bd8cfbd01641
Wesley Klop <wesley19097@gmail.com>
2021-02-05 10:41:33 +0100

    Scaffold y2020d14

---
+++ app/Solvers/Y2020/Day14/Solver.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Solvers\Y2020\Day14;
+
+
+use App\Contracts\Solution;
+
+class Solver extends \App\Solvers\AbstractSolver
+{
+
+    protected function solvePartOne(): Solution
+    {
+        // TODO: Implement solvePartOne() method.
+    }
+
+    protected function solvePartTwo(): Solution
+    {
+        // TODO: Implement solvePartTwo() method.
+    }
+}
