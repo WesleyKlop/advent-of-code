76226d9061cb9a3732e27d32adc22dbfec200abb
Wesley Klop <wesley19097@gmail.com>
2021-12-05 10:18:53 +0100

    2020 day 22 part 1

---
+++ app/Solvers/Y2020/Day22/Player.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Solvers\Y2020\Day22;
+
+use Illuminate\Support\Collection;
+use SplDoublyLinkedList;
+use SplQueue as Queue;
+
+class Player
+{
+    private Queue $cards;
+
+    public function __construct(private string $name, iterable $cards)
+    {
+        $this->cards = new Queue();
+        $this->cards->setIteratorMode(SplDoublyLinkedList::IT_MODE_DELETE | SplDoublyLinkedList::IT_MODE_FIFO);
+        foreach ($cards as $card) {
+            $this->cards->enqueue($card);
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function countCards(): int
+    {
+        return $this->cards->count();
+    }
+
+    public function popCard(): int
+    {
+        return $this->cards->dequeue();
+    }
+
+    public function addCards(int ...$cards): void
+    {
+        foreach ($cards as $card) {
+            $this->cards->enqueue($card);
+        }
+    }
+
+    public function score(): int {
+        $score = 0;
+        for($multiplier = $this->countCards(); $multiplier > 0; $multiplier--) {
+            $score += ($multiplier * $this->popCard());
+        }
+        return $score;
+    }
+}
---
+++ app/Solvers/Y2020/Day22/Solver.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Solvers\Y2020\Day22;
+
+use App\Contracts\Solution;
+use App\Exceptions\AnswerNotFoundException;
+use App\Solutions\PrimitiveValueSolution;
+use App\Solutions\TodoSolution;
+use App\Solvers\AbstractSolver;
+
+class Solver extends AbstractSolver
+{
+    /** @return Player[] */
+    private function getInput(): array
+    {
+        return $this->read('2020', '22')
+            ->explode("\n\n")
+            ->map(fn($player) => $this->parsePlayer($player))
+            ->all();
+    }
+
+    protected function solvePartOne(): Solution
+    {
+        [$player1, $player2] = $this->getInput();
+
+        while (true) {
+            $card1 = $player1->popCard();
+            $card2 = $player2->popCard();
+            if ($card1 > $card2) {
+                $player1->addCards($card1, $card2);
+            } else {
+                $player2->addCards($card2, $card1);
+            }
+
+            if ($player1->countCards() === 0) {
+                return new PrimitiveValueSolution($player2->score());
+            }
+            if ($player2->countCards() === 0) {
+                return new PrimitiveValueSolution($player1->score());
+            }
+        }
+
+        throw new AnswerNotFoundException();
+    }
+
+    protected function solvePartTwo(): Solution
+    {
+        return new TodoSolution();
+    }
+
+    private function parsePlayer(string $player): Player
+    {
+        $parsed = explode("\n", $player);
+        return new Player(
+            array_shift($parsed),
+            array_map('intval', $parsed),
+        );
+    }
+}
