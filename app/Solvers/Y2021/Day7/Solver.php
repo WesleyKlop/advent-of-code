0a0eca7a43e84ac4bc997f09544505c4f0abf961
Wesley Klop <wesley19097@gmail.com>
2021-12-07 11:08:15 +0100

    2021 day 7 part 2

--- app/Solvers/Y2021/Day7/Solver.php
+++ app/Solvers/Y2021/Day7/Solver.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 
 namespace App\Solvers\Y2021\Day7;
 
+use App\Contracts\HasProgressBar;
 use App\Contracts\Solution;
 use App\Solutions\PrimitiveValueSolution;
-use App\Solutions\TodoSolution;
 use App\Solvers\AbstractSolver;
 use Illuminate\Support\Collection;
+use Symfony\Component\Console\Helper\ProgressBar;
 
-class Solver extends AbstractSolver
+class Solver extends AbstractSolver implements HasProgressBar
 {
+    private ProgressBar $progressBar;
+
     private function getInput(): Collection
     {
         return $this->read('2021', '7')
@@ -22,20 +25,51 @@ class Solver extends AbstractSolver
 
     protected function solvePartOne(): Solution
     {
-        $input = $this->getInput();
-        // Move all the inputs to the median
-        $target = $input->median();
-        $cost = 0;
+        $crabPositions = $this->getInput();
+        $targetPosition = $crabPositions->median();
+        $totalFuelCost = 0;
 
-        foreach ($input as $crabPosition) {
-            $cost += abs($crabPosition - $target);
+        foreach ($crabPositions as $crabPosition) {
+            $totalFuelCost += abs($crabPosition - $targetPosition);
         }
 
-        return new PrimitiveValueSolution($cost);
+        return new PrimitiveValueSolution($totalFuelCost);
+    }
+
+    protected function getRange(Collection $collection): array
+    {
+        return range($collection->min(), $collection->max());
     }
 
     protected function solvePartTwo(): Solution
     {
-        return new TodoSolution();
+        $crabPositions = $this->getInput();
+        $bestCost = PHP_INT_MAX;
+
+        $range = $this->getRange($crabPositions);
+        $this->progressBar->start(count($range));
+        foreach ($range as $targetPosition) {
+            $cost = 0;
+            foreach ($crabPositions as $crabPosition) {
+                $cost += $this->calculateFuelCost($crabPosition, $targetPosition);
+            }
+            $bestCost = min($cost, $bestCost);
+            $this->progressBar->advance();
+        }
+        $this->progressBar->clear();
+
+        return new PrimitiveValueSolution($bestCost);
+    }
+
+    private function calculateFuelCost(int $crabPosition, int $targetPosition): int
+    {
+        $steps = abs($crabPosition - $targetPosition);
+        // Gaussian sum formula
+        return (int)($steps * ($steps + 1) / 2);
+    }
+
+    public function setProgressBar(ProgressBar $progressBar): void
+    {
+        $this->progressBar = $progressBar;
     }
 }
