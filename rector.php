0ded7834c7f444c9ed44f3c3186d1372342eb79c
Wesley Klop <wesley19097@gmail.com>
2022-10-20 13:56:34 +0200

    Update rector config and add fleet folder

--- rector.php
+++ rector.php
@@ -2,15 +2,14 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+return static function (\Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81
+    ]);
 
-    $parameters->set(Option::PATHS, [
+    $rectorConfig->paths([
         __DIR__ . '/app',
         __DIR__ . '/tests',
         __DIR__ . '/ecs.php',
@@ -18,16 +17,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/advent-of-code',
     ]);
 
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->skip([
         \Rector\Php70\Rector\StaticCall\StaticCallOnNonStaticToInstanceCallRector::class,
+        \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
     ]);
 
-    // auto import fully qualified class names? [default: false]
-    $parameters->set(Option::AUTO_IMPORT_NAMES, false);
-
-    // skip root namespace classes, like \DateTime or \Exception [default: true]
-    $parameters->set(Option::IMPORT_SHORT_CLASSES, true);
-
-    // skip classes used in PHP DocBlocks, like in /** @var \Some\Class */ [default: true]
-    $parameters->set(Option::IMPORT_DOC_BLOCKS, true);
+    $rectorConfig->importNames(
+        importNames: false,
+        importDocBlockNames: true
+    );
+    $rectorConfig->importShortClasses(importShortClasses: true);
 };
