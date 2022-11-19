6ad8aa01dcce3a27f755163f72170bfcf3b108b7
Wesley Klop <wesley19097@gmail.com>
2022-11-19 18:16:01 +0100

    Add more solvers

---
+++ app/Solvers/Y2021/Day14/Solver.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Solvers\Y2021\Day14;
+
+use App\Contracts\Solution;
+use App\Solutions\TodoSolution;
+use App\Solvers\AbstractSolver;
+use Illuminate\Support\Stringable;
+
+class Solver extends AbstractSolver
+{
+    private function getInput(): Stringable
+    {
+        return $this->read('2021', '14');
+    }
+
+    protected function solvePartOne(): Solution
+    {
+        return new TodoSolution();
+    }
+
+    protected function solvePartTwo(): Solution
+    {
+        return new TodoSolution();
+    }
+}
