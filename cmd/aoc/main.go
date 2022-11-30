35f197bf343e26514c1e9bc0af8052cd1c59d876
Wesley Klop <wesley19097@gmail.com>
2022-11-30 22:17:58 +0100

    Make a start

--- cmd/aoc/main.go
+++ cmd/aoc/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func getArgs() (year *int, day *int, err error) {
+	year = flag.Int("year", 2022, "The year to solve")
+	if *year != 2022 {
+		err = fmt.Errorf("invalid year %d", *year)
+		return nil, nil, err
+	}
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		err = fmt.Errorf("missing required argument day")
+		return nil, nil, err
+	}
+	args := flag.Args()
+
+	if daynum, err := strconv.Atoi(args[0]); err != nil {
+		err = fmt.Errorf("%s is not a valid number", args[0])
+		return nil, nil, err
+	} else {
+		day = &daynum
+	}
+
+	if *day < 1 || *day > 31 {
+		err = fmt.Errorf("invalid day %d", *day)
+		return nil, nil, err
+	}
+	return
+}
 
 func main() {
-    fmt.Println("Hello, world.")
+	year, day, err := getArgs()
+	l := log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
+
+	if err != nil {
+		l.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Executing day %d in %d\n", day, year)
+
 }
